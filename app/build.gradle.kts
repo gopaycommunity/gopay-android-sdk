4536d1f5d370df445bd489a0e2a4299de7e27194
Jiří Hauser <jiri.hauser@argo22.com>
2025-07-22 10:42:54 +0200

    chore: setup example app signing for internal distribution GPMOB-107

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,19 @@ android {
         }
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+    
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
